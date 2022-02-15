c1822cfc0fb39e8f66fadc08486c3a7da7b18276
Karolina <k.olszewskaaa@gmail.com>
2022-02-15 22:28:37 +0000

    Configure wemake-python-styleguide locally and fix issues

--- revsplit/cli.py
+++ revsplit/cli.py
@@ -4,4 +4,5 @@ import click
 @click.command()
 @click.option('--name')
 def hello(name: str):
-    click.echo(f"Hello {name}!")
+    """Dummy function for testing."""
+    click.echo(f'Hello {name}!')
